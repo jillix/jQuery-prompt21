85ac8e39f40507fad17fb97bee6e8a19abda161c
Ionică Bizău <bizauionica@yahoo.com>
2015-01-07 11:14:45 +0200

    Added gitignore and removed obsolete files.

--- demo/js/handlers.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var p = $(".popup").prompt21();
-    $(".show-popup").on("click", function () {
-        p.getData(function (err, data) {
-            $(".result").text(JSON.stringify(data, null, 2));
-        });
-    });
-    $(".version").text($.fn.prompt21.version);
-});
